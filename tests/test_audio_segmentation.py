730b9657f154337c387d08566f9bef13d7c81d98
Gabriele Calvo <gcalvo87@gmail.com>
2017-08-03 23:03:47 +0100

    started to port code to python 3 and making it PEP8 compliant

---
+++ tests/test_audio_segmentation.py
@@ -0,0 +1,12 @@
+from audioSegmentation import speaker_diarization
+import os
+import numpy
+
+numpy.random.seed(123)  # for reproducibility
+ROOT_FLD = os.path.dirname(os.path.dirname(__file__))
+
+
+def test_speaker_diarization_no_lda():
+    file_path = os.path.join(ROOT_FLD, 'data', 'diarizationExample.wav')
+    cls = speaker_diarization(filepath=file_path, num_of_speakers=4, lda_dim=0, plot_=True)
+    print(cls)
