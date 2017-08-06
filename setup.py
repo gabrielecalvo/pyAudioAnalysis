875a06ee4707f74c505a11d9df815a54a0cc633e
Gabriele Calvo <gcalvo87@gmail.com>
2017-08-06 15:39:24 +0100

    added package_data to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(
     long_description=open('README.md').read(),
     version='0.1dev',
     packages=['pyaudioanalysis'],
+    package_data={'pyaudioanalysis': ['resources/*']},
     url='https://github.com/gabrielecalvo/pyAudioAnalysis',
     license='LICENSE.md',
     install_requires=["numpy", "matplotlib", "scipy", "simplejson", "scikit-learn", "eyed3", "pydub", "hmmlearn"],
+    include_package_data=True,
 )
